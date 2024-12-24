a0a097deb12ffe3ecc1c436fe8d9b3126383ce5c
Samy Ben Tayeb <sben-tay@bess-f2r1s5.clusters.42paris.fr>
2024-12-24 02:39:44 +0100

    EXPAND ARGS OK !

--- src/expand/ft_expand_args.c
+++ src/expand/ft_expand_args.c
@@ -6,31 +6,44 @@
 /*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 09:09:13 by sben-tay          #+#    #+#             */
-/*   Updated: 2024/12/22 16:02:23 by sben-tay         ###   ########.fr       */
+/*   Updated: 2024/12/24 02:39:28 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minishell.h"
 
-char	*expand_dollar(char *str, int *i, t_envp *envp, int exit_status);
-char	*join_char_to_str(char *result, char c);
-void	remove_quotes(char *str);
-void	free_and_shift(char **args, int index);
+	char	*expand_dollar(char *str, int *i, t_envp *envp, int exit_status);
+	char	*join_char_to_str(char *result, char c);
+	void	remove_quotes(char *str);
+	void	free_and_shift(char **args, int index);
 
 char	*expand_variable_in_str(char *str, t_envp *envp, int exit_status)
 {
 	char	*result;
 	char	*tmp;
 	int		i;
+	char	in_quote;
 
 	result = ft_strdup("");
 	if (!result)
 		return (NULL);
 	i = 0;
+	in_quote = 0;
 	while (str[i])
 	{
-		if (str[i] == '$' && str[i + 1])
+		if ((str[i] == '\'' || str[i] == '\"'))
+		{
+			if (in_quote == 0)
+				in_quote = str[i];
+			else if (in_quote == str[i])
+				in_quote = 0;
+			result = join_char_to_str(result, str[i++]);
+			if (!result)
+				return (ft_free((void **)&result), NULL);
+			continue ;
+		}
+		if (str[i] == '$' && in_quote != '\'') // Ne pas expanser si dans single quotes
 		{
 			tmp = expand_dollar(str, &i, envp, exit_status);
 			if (!tmp)
@@ -46,28 +59,28 @@ char	*expand_variable_in_str(char *str, t_envp *envp, int exit_status)
 	return (result);
 }
 
-int	ft_expand_args(char **args, t_envp *envp, int exit_status)
-{
-	int		i;
-	char	*expanded;
-
-	if (!args || !*args)
-		return (SUCCESS);
-	i = 0;
-	while (args[i])
+	int	ft_expand_args(char **args, t_envp *envp, int exit_status)
 	{
-		expanded = expand_variable_in_str(args[i], envp, exit_status);
-		if (!expanded)
-			return (ERROR);
-		ft_free((void **)&args[i]);
-		args[i] = expanded;
-		remove_quotes(args[i]);
-		if (!args[i][0])
+		int		i;
+		char	*expanded;
+
+		if (!args || !*args)
+			return (SUCCESS);
+		i = 0;
+		while (args[i])
 		{
-			free_and_shift(args, i);
-			continue ;
+			expanded = expand_variable_in_str(args[i], envp, exit_status);
+			if (!expanded)
+				return (ERROR);
+			ft_free((void **)&args[i]);
+			args[i] = expanded;
+			remove_quotes(args[i]);
+			if (!args[i][0])
+			{
+				free_and_shift(args, i);
+				continue ;
+			}
+			i++;
 		}
-		i++;
+		return (SUCCESS);
 	}
-	return (SUCCESS);
-}
